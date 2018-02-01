61e04eda78a68d133787ce26b110553e849c0ca9
ikuisma <ilkka.a.kuisma@gmail.com>
2018-02-01 11:30:20 +0200

    Finished exercise 3.3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-let notes = [
+let persons = [
     {name: "Arto Hellas", number: "040-123456", id: 1},
     {name: "Martti Tienari", number: "040-123456", id: 2},
     {name: "Arto Järvinen", number: "040-123456", id: 3},
@@ -9,12 +9,22 @@ let notes = [
 ]
 
 app.get('/api/persons', (req, res) => {
-    res.json(notes)
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id == id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
 })
 
 app.get('/info', (req, res) => {
     res.send(`
-        <p>puhelinluettelossa on ${notes.length} henkilön tiedot</p>
+        <p>puhelinluettelossa on ${persons.length} henkilön tiedot</p>
         <p>${new Date()}</p>
     `)
 })
