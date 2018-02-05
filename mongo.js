ce863518ba45d78977b5b3deeaa5f11299d8b297
ikuisma <ilkka.a.kuisma@gmail.com>
2018-02-05 09:51:46 +0200

    Finished exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const dbuser = ''
+const dbpass = ''
+const url = `mongodb://${dbuser}:${dbpass}@ds125388.mlab.com:25388/fullstack-persons`
+
+mongoose.connect(url)
+
+const Person = mongoose.model('Person', {
+    'name': String,
+    'number': String
+})
+
+const savePerson = (name, number) => {
+    const person = new Person({name, number})
+    person.save().then(result => {mongoose.connection.close()})
+} 
+
+const printPersons = () => {
+    console.log('puhelinluettelo')
+    Person.find({}).then(result => {
+        result.forEach(p => console.log(p.name, p.number))
+        mongoose.connection.close()
+    })
+}
+
+const addPerson = (name, number) => {
+    console.log(`lisätään henkilö ${name} numero ${number} luetteloon`)
+    savePerson(name, number)
+}
+
+const main = () => {
+    [ , , ...personParams] = process.argv 
+    if (personParams.length === 0) {
+        printPersons()
+    } else {
+        [name, number] = personParams
+        addPerson(name, number)
+    }
+}
+
+main()
