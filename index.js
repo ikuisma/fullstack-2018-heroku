2f4ef2877b376b257553dc480c8fc7f7a17f0d8f
ikuisma <ilkka.a.kuisma@gmail.com>
2018-02-06 09:43:22 +0200

    Finished exercises 3.13 - 3.19

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
+const Person = require('./models/person')
 
 const idMax = 10000
 const PORT = process.env.PORT || 3001
@@ -14,57 +15,96 @@ app.use(bodyParser.json())
 app.use(morgan(':method :url :data :status :res[content-length] - :response-time ms : '))
 app.use(cors())
 
-let persons = [
-    {name: "Arto Hellas", number: "040-123456", id: 1},
-    {name: "Martti Tienari", number: "040-123456", id: 2},
-    {name: "Arto Järvinen", number: "040-123456", id: 3},
-    {name: "Lea Kutvonen", number: "040-123456", id: 4}
-]
+let persons = []
 
-const stripId = (request) => Number(request.params.id)
+const userWithNameExists = (name) => {
+    Person.find({name}).then()
+    return false
+}
+
+const stripId = (request) => request.params.id
 
 const randomId = () => Math.floor(Math.random() * idMax)
 
-const validationErrors = (person) => {
-    const errors = []
-    if (!person.name || !person.number) {
-        errors.push('Missing name or number. ')
-    } else if (persons.find(p => p.name == person.name)) {
-        errors.push('Name must be unique')
-    }
-    return errors
-}
-
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person
+        .find({})
+        .then(p => {
+            res.json(p.map(Person.format))
+        })
 })
 
 app.post('/api/persons', (req, res) => {
-    const person = {...req.body}
-    const errors = validationErrors(person)
-    if (errors.length === 0) {
-        person.id = randomId()
-        persons = persons.concat(person)
-        res.json(person)
-    } else {
-        res.status(400).json({errors})
-    }
+    const data = {...req.body}
+    errors = []
+    Person
+        .findOne({name: data.name})
+        .then(person => {
+            if (person) {
+                return Promise.reject('Person with name already exists')
+            } else {
+                return data
+            }
+        })
+        .then(data => {
+            if (!data.name || !data.number) {
+                return Promise.reject('Missing name or number')
+            } else {
+                return data
+            }
+        })
+        .then(data => {
+            const person = new Person(data);
+            person
+                .save()
+                .then(Person.format)
+                .then(person => {
+                    res.json(person)
+                })
+        }).catch(error => {
+            res.status(400).send({error})
+        })
 })
+
+app.put('/api/persons/:id', (req, res) => {
+    const id = stripId(req)
+    const person = {
+        name: req.body.name,
+        number: req.body.number
+    }
+    Person
+        .findByIdAndUpdate(id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(Person.format(updatedPerson))
+        })
+        .catch(error => {
+            res.status(400).send({ error: 'malformatted id. '})
+        })
+}) 
 
 app.get('/api/persons/:id', (req, res) => {
     const id = stripId(req)
-    const person = persons.find(p => p.id === id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    Person.findById(id).then( person => {
+        if (person) {
+            res.json(Person.format(person))
+        } else {
+            res.status(404).end()
+        }
+    }).catch(error => {
+        res.status(400).send({ error: 'malformatted id. '})
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    id = stripId(req)
-    persons = persons.filter(person => person.id !== id)
-    res.status(204).end()
+    const id = stripId(req)
+    Person
+        .findByIdAndRemove(id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => {
+            res.status(400).send({error: 'malformatted id. '})
+        })
 })
 
 app.get('/info', (req, res) => {
---
+++ models/person.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const dbuser = ''
+const dbpass = ''
+const url = `mongodb://${dbuser}:${dbpass}@ds125388.mlab.com:25388/fullstack-persons`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    'name': String,
+    'number': String
+})
+
+personSchema.statics.format = (person) => ({        
+    id: person._id,
+    name: person.name,
+    number: person.number
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+module.exports = Person
